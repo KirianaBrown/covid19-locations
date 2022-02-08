c3f87e7cc48095034f96ed2bcc0cb12104a81aaf
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-09 11:21:19 +1300

    set up useSelector to get state location data from store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLocationData } from "./store/locations-action";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchLocationData());
+  }, [dispatch]);
+
+  const locationData = useSelector((state) => state.locations.isLoaded);
+
+  useEffect(() => {
+    locationData && setHasLoaded(true);
+  }, [locationData]);
+
+  return (
+    <div>
+      <Header />
+      {!hasLoaded && <p>Loading ...</p>}
+      {hasLoaded && <h1>Loaded Data</h1>}
+    </div>
+  );
 }
 
 export default App;
