60e2e4410a85b23428ef13b0c9715af1ff206e16
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-08 16:23:22 +1300

    set up header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Covid-19. New Zealand Locations</h1>
+    </header>
+  );
+}
