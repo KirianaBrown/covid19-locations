0e103a1daad0df5f4b5767650307eebb0c7ae0b3
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-09 16:13:15 +1300

    outline data for locations table including exposure risk img

--- src/components/Locations/LocationsTable.jsx
+++ src/components/Locations/LocationsTable.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Close from "../../assets/close.png";
+import Casual from "../../assets/casual.png";
 import classes from "./LocationsTable.module.css";
 
 export default function LocationsTable() {
   return (
     <div className={classes.locationsTable}>
-      <p>Table</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>Date</th>
+            <th>Exposure Level</th>
+            <th>Published</th>
+            <th>Advice</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Kens Mart</td>
+            <td>2022-08-01 NZDT - 2022-08-09 NZDT</td>
+            <td>
+              <img src={Casual} alt="Close Contact" />
+            </td>
+            <td>2022-08-01 NZDT</td>
+            <td>Link</td>
+          </tr>
+          <hr></hr>
+        </tbody>
+      </table>
       <div className={classes.pagination}>Pagination</div>
     </div>
   );
