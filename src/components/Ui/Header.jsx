53711808b5830d344d71743d74025d1f02ba3db0
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-11 12:42:31 +1300

    set link to represent govt resources

--- src/components/Ui/Header.jsx
+++ src/components/Ui/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header>
       <h1>Covid-19. New Zealand Locations</h1>
-      <a href="www.google/com" className={classes.link}>
-        Link
+      <a href="https://covid19.govt.nz/" className={classes.link}>
+        Unite Against Covid-19
       </a>
     </header>
   );
