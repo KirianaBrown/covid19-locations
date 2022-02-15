0cb340d6a9c660bc1dba8657d62bb8baec63d7c9
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-15 14:40:36 +1300

    add logo to header

--- src/components/Ui/Header.jsx
+++ src/components/Ui/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
 import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
-      <h1>Covid-19. New Zealand Locations</h1>
+      <div>
+        <img src={Logo} alt="logo blue circle" />
+        <h1>Covid-19 . New Zealand Locations</h1>
+      </div>
       <a href="https://covid19.govt.nz/" className={classes.link}>
         Unite Against Covid-19
       </a>
