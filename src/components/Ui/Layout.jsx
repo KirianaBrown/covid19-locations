280921ccf935a0419f35381f9ad978cf47a4e325
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-15 13:19:45 +1300

    create footer and import into layout component

--- src/components/Ui/Layout.jsx
+++ src/components/Ui/Layout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Footer from "./Footer";
 import classes from "./Layout.module.css";
 
 export default function Layout(props) {
-  return <div className={classes.layout}>{props.children}</div>;
+  return (
+    <div>
+      <main className={classes.layout}>{props.children}</main>
+      <Footer />
+    </div>
+  );
 }
