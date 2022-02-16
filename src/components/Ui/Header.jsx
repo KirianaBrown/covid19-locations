ac351a2e0f705ea1ef5401a074c2fecfe109f31e
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-02-17 11:49:03 +1300

    show latest publishedAt as latest update in header

--- src/components/Ui/Header.jsx
+++ src/components/Ui/Header.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Logo from "../../assets/logo.png";
 import classes from "./Header.module.css";
 
 export default function Header() {
+  const allLocations = useSelector((state) => state.locations.locations.items);
+
+  const latestPublishedDate = allLocations.reduce((a, b) =>
+    a.publishedAt > b.publishedAt ? a : b
+  );
+
   return (
     <header>
       <div>
         <img src={Logo} alt="logo blue circle" />
         <h1>Covid-19 . New Zealand Locations</h1>
       </div>
+      <p>
+        Last Updated:
+        {new Date(latestPublishedDate.publishedAt).toString()}
+      </p>
       <a href="https://covid19.govt.nz/" className={classes.link}>
         Unite Against Covid-19
       </a>
