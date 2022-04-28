5d8d9a49c7cd388b9e77b5bb5557f50919a7c985
KirianaBrown <kjb0369@arastudent.ac.nz>
2022-04-28 15:45:16 +1200

    rm footer section from layout

--- src/components/Ui/Layout.jsx
+++ src/components/Ui/Layout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import classes from "./Layout.module.css";
 
 export default function Layout(props) {
   return (
     <div>
       <main className={classes.layout}>{props.children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
